ab243d91627305ce427633cdec66612598924708
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2023-01-21 10:10:19 +0500

    Servlet WebApp

--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -1,5 +1,19 @@
 package com.example.servlet;
 
-public class GetUsersServlet {
+import com.example.Warehouse;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class GetUsersServlet extends HttpServlet {
     //write your code here!
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("users", Warehouse.getInstance().getUsers());
+        getServletContext().getRequestDispatcher("/jsp/users.jsp").forward(req, resp);
+    }
 }
